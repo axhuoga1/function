1ad44dd94e91b2aff7e58537c0865024e3b0d08d
axhuoga1 <axihuoga@gmail.com>
2024-11-09 12:10:27 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "yui.h"
+using namespace std;
+int main(){
+        int a,b;
+        cin>>a>>b;
+        cout<<jia(a,b)<<endl;
+        cout<<jian(a,b)<<endl;
+        cout<<cheng(a,b)<<endl;
+        cout<<cu(a,b)<<endl;
+        return 0;
+}
